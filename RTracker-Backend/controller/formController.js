5029aec35acb36a561430be4b95d9ede4820cdfd
Ricardo Granadeño <ricardosliver2@gmail.com>
2022-03-30 21:59:40 -0600

    *Backend* - Se configuro el endpoint de form-collection - Se configuro el cord
    *Frontend*
    - Se configuro el axios (Basico), para hacer peticiones al backend
    - Se quito la informacion dummy
    - Se corrigio un erro para cuando fields esta vacio

--- RTracker-Backend/controller/formController.js
+++ RTracker-Backend/controller/formController.js
@@ -1,9 +1,14 @@
 var appFirebase = require("../firebase/firebase");
+
 const db = appFirebase.firestore();
+
 exports.GetFormByDoc = async function (req, res) {
-  const fields = db.collection("form-collection").doc("avNXK0iEY9URfqlCLI1s");
+  const fields = db.collection("form-collection").doc(req.params.id);
   const doc = await fields.get();
 
   // res.status(400).send({ error: error.details[0].message });
-  res.status(200).send(doc);
+  res.json({
+    success: true,
+    response: doc.data(),
+  });
 };
--- RTracker-Frontend/src/components/views/formDynamic/index.jsx
+++ RTracker-Frontend/src/components/views/formDynamic/index.jsx
@@ -1,4 +1,6 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
+import SettingFrom from './settings'
+import FormMethod from '../../../apis/rtracker-api'
 import Box from '@mui/material/Box';
 import InputMapper from '../../molecules/inputMapper';
 import Card from '@mui/material/Card';
@@ -7,102 +9,10 @@ import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import SettingsIcon from '@mui/icons-material/Settings';
-import SettingFrom from './settings'
 
 const FormDynamic = () => {
     const SettingRef = useRef();
-    const [DynamicForm, setDynamicForm] = React.useState({
-        Fields: [
-            {
-                idField: 1,
-                NameField: 'scrtUser',
-                LabelField: 'Suscription User',
-                Type: 'Dropdown',
-                PlaceholderInput: 'Select Suscription User',
-                Value: '',
-                Options: [
-                    {
-                        Value: 15,
-                        Text: 'Costo $15'
-                    },
-                    {
-                        Value: 25,
-                        Text: 'Costo $25'
-                    },
-                    {
-                        Value: 35,
-                        Text: 'Costo $35'
-                    }
-                ],
-                Attribut: '',
-                CascadeTrigger: false,
-                Cascade: [],
-                Show: true
-            },
-            {
-                idField: 2,
-                NameField: 'textNameUser',
-                LabelField: 'Name User',
-                Type: 'TextBox',
-                PlaceholderInput: 'Escriba el nombre del empleado',
-                Attribut: '',
-                Value: '',
-                Options: [],
-                CascadeTrigger: false,
-                Cascade: [],
-                Show: true
-            },
-            {
-                idField: 3,
-                NameField: 'textUbi',
-                LabelField: 'Vivienda',
-                Type: 'TextBox',
-                PlaceholderInput: 'Escriba la ubicacion del empleado',
-                Attribut: '',
-                Value: '',
-                Options: [],
-                CascadeTrigger: false,
-                Cascade: [],
-                Show: true
-            },
-            {
-                idField: 4,
-                NameField: 'numhabitantes',
-                LabelField: 'Numero de habitantes',
-                Type: 'Number',
-                PlaceholderInput: 'Escriba el numero de personas que viven con usted',
-                Attribut: '',
-                Value: '',
-                Options: [],
-                CascadeTrigger: false,
-                Cascade: [],
-                Show: true
-            },
-            {
-                idField: 5,
-                NameField: 'drpdwnCost',
-                LabelField: 'Tiempo de pago',
-                Type: 'Dropdown Cascade',
-                PlaceholderInput: 'Elija la suscripcion del empleado',
-                Attribut: '',
-                Value: '',
-                Options: [
-                    {
-                        id: 1, Value: 45, Text: 'Precio 15$', Calling: [6]
-                    },
-                    {
-                        id: 2, Value: 46, Text: 'Precio 25$', Calling: [7]
-                    },
-                    {
-                        id: 3, Value: 47, Text: 'Precio 35$', Calling: [8, 9, 10, 11, 7, 6]
-                    }
-                ],
-                CascadeTrigger: true,
-                Cascade: [],
-                Show: true
-            }
-        ]
-    });
+    const [DynamicForm, setDynamicForm] = React.useState({ Fields: [] });
 
     const addCascadeInputs = (IdInputs, Children) => {
 
@@ -163,6 +73,17 @@ const FormDynamic = () => {
         // setDynamicForm(SettingRef.current.getJsonRender()));
 
     }
+
+    //firth data load
+    useEffect(() => {
+        // FormMethod.Form.GetFields("avNXK0iEY9URfqlCLI1s").then((res) => {
+           
+        //     setDynamicForm(res.data.response || { Fields: [] });
+        // }).catch((err) => {
+        //     console.log(err)
+        //     //TODO: Message error
+        // });
+    });
     return (
         <Box
             component="div">
@@ -188,25 +109,26 @@ const FormDynamic = () => {
                         }}
                         autoComplete="off">
                         {/* Input  */}
-                        {DynamicForm.Fields.map((field, key) => {
-                            let render = [];
-                            render.push(
-                                <div key={`Div-${field.idField}`} >
-                                    <InputMapper key={key} properties={field} addCascadeInputs={addCascadeInputs} handleValue={handleValue} />
-                                </div>);
+                        {DynamicForm.Fields.length > 0 &&
+                            DynamicForm.Fields.map((field, key) => {
+                                let render = [];
+                                render.push(
+                                    <div key={`Div-${field.idField}`} >
+                                        <InputMapper key={key} properties={field} addCascadeInputs={addCascadeInputs} handleValue={handleValue} />
+                                    </div>);
 
-                            // Cascade Input
-                            if (field.CascadeTrigger && field.Cascade.length > 0) {
+                                // Cascade Input
+                                if (field.CascadeTrigger && field.Cascade.length > 0) {
 
-                                field.Cascade.forEach((fieldCascade, key_fieldCascade) => {
-                                    render.push(
-                                        <div key={`DivCascade-${field.idField}`} >
-                                            <InputMapper key={key_fieldCascade} properties={fieldCascade} addCascadeInputs={addCascadeInputs} handleValue={handleValue} />
-                                        </div>)
-                                })
-                            };
-                            return render
-                        })}
+                                    field.Cascade.forEach((fieldCascade, key_fieldCascade) => {
+                                        render.push(
+                                            <div key={`DivCascade-${field.idField}`} >
+                                                <InputMapper key={key_fieldCascade} properties={fieldCascade} addCascadeInputs={addCascadeInputs} handleValue={handleValue} />
+                                            </div>)
+                                    })
+                                };
+                                return render
+                            })}
                     </Box >
                     <Typography variant="body2" color="text.secondary">
                         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat consequatur magni asperiores incidunt ratione nisi, omnis reiciendis, eveniet culpa veritatis, tempore fugiat illum mollitia quidem?
